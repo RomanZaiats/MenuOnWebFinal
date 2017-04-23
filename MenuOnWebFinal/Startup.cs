4da83753e514ba472133155fd6ee2c8bbcdc0d1d
Roman Zajats <romzayac@gmail.com>
2017-04-23 15:28:24 +0300

    Add project files.

---
+++ MenuOnWebFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MenuOnWebFinal.Startup))]
+namespace MenuOnWebFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
